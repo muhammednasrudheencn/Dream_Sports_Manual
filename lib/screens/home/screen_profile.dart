9304206077a7229b0de6df818e559e9f99c1dd05
M.NASRU <nasrudheenkarimba@gmail.com>
2023-10-13 21:00:56 +0530

    fifth day turf list

--- lib/screens/home/screen_profile.dart
+++ lib/screens/home/screen_profile.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dream_sports_turf_owner/constants/colors.dart';
+import 'package:dream_sports_turf_owner/services/firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,6 +8,40 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final mediaquery = MediaQuery.of(context).size;
+    return Scaffold(
+      body: StreamBuilder(
+        stream: store
+            .collection('TurfOwner')
+            .doc(auth.currentUser!.uid)
+            .collection('images')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          String url = snapshot.data!.docs[0]['turfimage'];
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text('Loading'),
+            );
+          } else {
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: mediaquery.width,
+                      height: mediaquery.height * 0.25,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(url), fit: BoxFit.cover)),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
